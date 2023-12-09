1414b3afb2af597dfbdf35c6e4943a6860bd143e
Nguyen Truong <binclct@gmail.com>
2023-12-09 20:43:44 +0700

    - Update /api/admin/employee to handle not existed user

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Req } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req } from "@nestjs/common";
 import { AdminService } from "./admin.service";
 import { DeleteEmployeeRequest, InsertEmployeeRequest, TopNCustomersRequest, TopNProductiveEmployeesRequest, UpdateEmployeeRequest } from "./dto";
 
@@ -7,36 +7,43 @@ export class AdminController {
     constructor(private adminService: AdminService) {}
 
     @Post('/add-employee')
+    @HttpCode(200)
     insertEmployee(@Body() request: InsertEmployeeRequest) {
         return this.adminService.insertEmployee(request)
     }
 
     @Post('/update-employee')
+    @HttpCode(200)
     updateEmployee(@Body() request: UpdateEmployeeRequest) {
         return this.adminService.updateEmployee(request)
     }
 
     @Post('/delete-employee')
+    @HttpCode(200)
     deleteEmployee(@Body() request: DeleteEmployeeRequest) {
         return this.adminService.deleteEmployee(request.username)
     }
 
     @Post('/top-n-vip-customers')
+    @HttpCode(200)
     getTopNVipCusotmers(@Body() request: TopNCustomersRequest) {
         return this.adminService.topVipCustormers(request)
     }
 
     @Post('/top-n-productive-employees')
+    @HttpCode(200)
     getTopNProductiveEmployees(@Body() request: TopNProductiveEmployeesRequest) {
         return this.adminService.topProductiveEmployee(request)
     }
 
     @Get('/employees')
+    @HttpCode(200)
     getEmployees() {
         return this.adminService.getEmployees()
     }
 
     @Post('/employee')
+    @HttpCode(200)
     getEmployeeDetail(@Body("username") username: string) {
         return this.adminService.getEmployeeDetail(username)
     }
